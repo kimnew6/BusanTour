d5f60e6effb0df4742eeacfaa4523fc66911bdec
kimnew6 <kimnew6@gmail.com>
2022-04-25 17:09:34 +0900

    add: KAKAO MAP

--- pages/hotplaces/[...params].js
+++ pages/hotplaces/[...params].js
@@ -1,12 +1,48 @@
 import { useRouter } from "next/router";
 import Image from "next/image";
 import Seo from "../../components/Seo";
+import { useEffect } from "react";
+
 const API_KEY = process.env.API_KEY;
+const NEXT_PUBLIC_KAKAOMAP_APPKEY = process.env.NEXT_PUBLIC_KAKAOMAP_APPKEY;
+
 export default function Detail({ data, params }) {
   const router = useRouter();
   console.log(data);
   const detail = data.getFoodKr.item[0];
   const [title, id] = params || [];
+
+  useEffect(() => {
+    const mapScript = document.createElement("script");
+
+    mapScript.async = true;
+    mapScript.src = `//dapi.kakao.com/v2/maps/sdk.js?appkey=${NEXT_PUBLIC_KAKAOMAP_APPKEY}&autoload=false`;
+
+    document.head.appendChild(mapScript);
+
+    const onLoadKakaoMap = () => {
+      window.kakao.maps.load(() => {
+        const container = document.getElementById("map");
+        const options = {
+          center: new window.kakao.maps.LatLng(detail.LAT, detail.LNG),
+          level: 3,
+        };
+        const map = new window.kakao.maps.Map(container, options);
+        const markerPosition = new window.kakao.maps.LatLng(
+          detail.LAT,
+          detail.LNG
+        );
+        const marker = new window.kakao.maps.Marker({
+          position: markerPosition,
+        });
+        marker.setMap(map);
+      });
+    };
+    mapScript.addEventListener("load", onLoadKakaoMap);
+
+    return () => mapScript.removeEventListener("load", onLoadKakaoMap);
+  }, [detail.LAT, detail.LNG]);
+
   return (
     <div>
       <Seo title={title} />
@@ -25,9 +61,11 @@ export default function Detail({ data, params }) {
       <span className="openingHours">
         영업 시간 : {detail.USAGE_DAY_WEEK_AND_TIME}
       </span>
-      <span className="contactNumber" style={{ marginBottom: "100px" }}>
-        전화 번호 : {detail.CNTCT_TEL}
-      </span>
+      <span className="contactNumber">전화 번호 : {detail.CNTCT_TEL}</span>
+      <div
+        id="map"
+        style={{ width: "520px", height: "300px", margin: "50px 0" }}
+      ></div>
       <style jsx>{`
         span {
           display: block;
